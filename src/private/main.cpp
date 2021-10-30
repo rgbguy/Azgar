03a0443f0ce06e17b78ff130f09648be528e7c0a
rgbguy <sagarkalbande1993@gmail.com>
2021-10-31 02:56:03 +0530

    Updated CMakeLists to include glfw. Specifying out of tree (add subdir) in cmake needs both src and build to be specified. Added main.cpp with glfw triangle code.

--- src/private/main.cpp
+++ src/private/main.cpp
@@ -1,7 +1,45 @@
+#define GL_SILENCE_DEPRECATION
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main(void)
 {
-    std::cout << "Hello World!\n";
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClearColor(0.2,0.2,0.2,1);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+        glColor3f(1, 0.2, 0.3);
+        glVertex3f(0, 0, 0);
+        glVertex3f(1, 0, 0);
+        glVertex3f(0, 1, 0);
+        glEnd();
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
